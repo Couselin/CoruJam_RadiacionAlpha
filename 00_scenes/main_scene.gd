d2069ca81edff5ecb665f81a1708ff4dd90af43c
Noa <noamagacouselo@gmail.com>
2026-06-07 14:28:00 +0200

    Tenemos sistema de dialogos (solo falta implementarlos)

--- 00_scenes/main_scene.gd
+++ 00_scenes/main_scene.gd
@@ -1,11 +1,17 @@
 extends Node2D
 
 @export var DeliverySlot: BasicSlot
-
-
+@export var character: Character
+@export var cajaDialogos: DialogoBasic
+@export var dialogueNumber: int = 0
 # BEGIN PLAY
 func _ready() -> void:
 	DeliverySlot.itemOnSlot.connect(checkDelivery)
+	#character.cambiarImagen(AA)
+	cajaDialogos.cambiarNombre("El hombre sonriente dice:")
+	cajaDialogos.cambiarDialogo("¡Jojojo! Dios os guarde joven. Verá... Estoy buscando algo que darle a mi escribano donde pueda apuntar a ciertos... Desvergonzados que se niegan a pagarme el diezmo ¡Jojojo! ¿conocéis algo que pueda servirme?")
+	cajaDialogos.animate_label()
+	dialogueNumber=1
 	pass 
 
 
@@ -17,6 +23,24 @@ func _process(_delta: float) -> void:
 #Detectar pedido correcto
 func checkDelivery():
 	if !DeliverySlot.SlotEmpty:
-		if DeliverySlot.ItemReference.ItemID == "azul":
-			print("item correcto")
+		if dialogueNumber==1:
+			if DeliverySlot.ItemReference.ItemID == "azul":
+				print("item correcto")
+				character.cambiarImagen(character.EB)
+				cajaDialogos.cambiarDialogo("¡Pardiez, esto es justo lo que necesitaba! ¿Cómo, un pagamento? ¿Cree usted en el santísimo, joven? ¡Pues el hombre propone y Dios dispone! Jojojo ¡Nos vemos!")
+				cajaDialogos.animate_label()
+				dialogueNumber=dialogueNumber+1
+				await get_tree().create_timer(5).timeout
+				siguienteDialogo(character.dardo,"Dardo dice:","puto")
+			else:
+				cajaDialogos.cambiarDialogo("Eres gilipollas.")
+				cajaDialogos.animate_label()
+		elif dialogueNumber==2:
 			pass
+
+func siguienteDialogo(sprite,nombre,dialogo):
+	character.cambiarImagen(sprite)
+	cajaDialogos.cambiarNombre(nombre)
+	cajaDialogos.cambiarDialogo(dialogo)
+	cajaDialogos.animate_label()
+	pass
